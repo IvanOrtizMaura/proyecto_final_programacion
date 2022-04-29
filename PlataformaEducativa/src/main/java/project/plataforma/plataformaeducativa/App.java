625584daf9da2da3951791a32e71ddef298ae438
Rodrigo García <rgarcia9@nigul.cide.es>
2022-04-29 18:46:20 +0200

    Login 1

--- PlataformaEducativa/src/main/java/project/plataforma/plataformaeducativa/App.java
+++ PlataformaEducativa/src/main/java/project/plataforma/plataformaeducativa/App.java
@@ -6,15 +6,32 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class App extends Application {
+
+    public static Connection dbCon;
+    public static String BDD = "edu";
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Pantalla_Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Eduform");
         stage.setScene(scene);
+        BDDConnection();
         stage.show();
+    }
+
+    public void BDDConnection(){
+        try{
+           dbCon = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "cacaca");
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- PlataformaEducativa/src/main/java/project/plataforma/plataformaeducativa/LoginController.java
+++ PlataformaEducativa/src/main/java/project/plataforma/plataformaeducativa/LoginController.java
@@ -3,6 +3,10 @@ package project.plataforma.plataformaeducativa;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class LoginController {
 
     @FXML
@@ -11,6 +15,31 @@ public class LoginController {
     @FXML
     private TextField pin;
 
+    @FXML
+    public void Acceder(){
 
+        Statement stat = null;
+        String query = "select * from " + App.BDD + ".alumno where dni = '" + usuario.getText() + "';";
 
+        try{
+            stat = App.dbCon.createStatement();
+            ResultSet rs = stat.executeQuery(query);
+            if(rs.next()){
+
+                if(rs.getString("pin") == pin.getText()){
+                    pin.setText("Login correcto");
+                }
+                else{
+                    pin.setText("Contraseña incorrecta");
+                }
+            }
+            else{
+                pin.setText("Login incorrecto");
+            }
+        }
+        catch (SQLException e){
+            e.getMessage();
+        }
+
+    }
 }
