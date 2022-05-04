540578207cebd1f0344abfeaff4afe47e97b769f
Rodrigo García <rgarcia9@nigul.cide.es>
2022-05-04 16:16:58 +0200

    Disabled TextField FocusTraversable on InicialController.

--- PlataformaEducativa/src/main/java/project/plataforma/plataformaeducativa/InicialController.java
+++ PlataformaEducativa/src/main/java/project/plataforma/plataformaeducativa/InicialController.java
@@ -31,11 +31,17 @@ public class InicialController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         infoDni.setText(LoginController.alumnoLogueado.getDni());
+        infoDni.setFocusTraversable(false);
         infoNombre.setText(LoginController.alumnoLogueado.getNombre());
+        infoNombre.setFocusTraversable(false);
         infoApellido1.setText(LoginController.alumnoLogueado.getApellido1());
+        infoApellido1.setFocusTraversable(false);
         infoApellido2.setText(LoginController.alumnoLogueado.getApellido2());
+        infoApellido2.setFocusTraversable(false);
         infoNacimiento.setText(LoginController.alumnoLogueado.getNacimiento());
+        infoNacimiento.setFocusTraversable(false);
         infoTelefono.setText(LoginController.alumnoLogueado.getTelefono());
+        infoTelefono.setFocusTraversable(false);
 
     }
 }
