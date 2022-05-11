7e46e8cffa99de9bdbc557713b7f0cef0b704e4b
IvanOrtizMaura <Ivantiz45@gmail.com>
2022-05-11 18:57:49 +0200

    CentroController

--- PlataformaEducativa/src/main/java/project/plataforma/plataformaeducativa/CentroController.java
+++ PlataformaEducativa/src/main/java/project/plataforma/plataformaeducativa/CentroController.java
@@ -3,13 +3,20 @@ package project.plataforma.plataformaeducativa;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class CentroController implements Initializable {
 
+    @FXML
+    private TextField infoNombreCentro, infoTelefonoCentro, infoDireccionCentro;
     @FXML
     private Button Pantalla_Asignaturas, Pantalla_Inicial, Pantalla_Curso, Pantalla_Profesores;
 
@@ -21,6 +28,26 @@ public class CentroController implements Initializable {
                 Pantalla_Curso, Pantalla_Profesores));
 
         Ventana.gestionarVentanas(ventanas);
+
+    }
+    public void consultarInfoProfesor(){
+        Statement stat = null;
+        String query = "select" + App.BDD + ".centro.* from " + App.BDD + ".centro" +
+                "inner join" +
+                App.BDD + ".alumno" +
+                "on "+ App.BDD+".centro.id = " + App.BDD + ".alumno.id_centro;";
+        try {
+            stat = App.dbCon.createStatement();
+            ResultSet rs = stat.executeQuery(query);
+            if (rs.next()) {
+                infoNombreCentro.setText(rs.getString("nombre"));
+                infoTelefonoCentro.setText(rs.getString("telefono"));
+                infoDireccionCentro.setText(rs.getString("direccion"));
+
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
 
     }
 }
